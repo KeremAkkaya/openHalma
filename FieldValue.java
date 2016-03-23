a1209055e0ba1de8da685bfe52eb4e74b35788fb
gammelon <tempmailmah@web.de>
2016-03-23 19:07:05 +0100

    unfinished changes, added new class, the second

---
+++ FieldValue.java
@@ -0,0 +1,11 @@
+
+/**
+ * Enumeration class Field - write a description of the enum class here
+ * 
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum FieldValue
+{
+    INVALID,EMPTY,PLAYER1,PLAYER2,PLAYER3,PLAYER4,PLAYER5,PLAYER6,PLAYER7;
+}
---
+++ StarBoard.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class StarBoard here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class StarBoard extends Board
+{
+    
+    public StarBoard()
+    {
+        
+    }
+
+    
+}
