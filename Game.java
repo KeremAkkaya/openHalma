c7f37dae83af448cb7eaf920de3d9bfacee523dd
gammelon <tempmailmah@web.de>
2016-03-27 22:01:59 +0200

    added a game-class

---
+++ Game.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+/**
+ * Write a description of class Game here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Game
+{
+    private Board board;
+    private LinkedList<Move> moves = new LinkedList<>();
+    public Game()
+    {
+        
+    }
+
+    
+}
